ae0647e9ac3c840a69c5471464d081b7b204cf02
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-24 14:56:49 -0500

    Add dates created to rentals

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,12 +2,20 @@ class Rental < ActiveRecord::Base
     belongs_to :vhs
     belongs_to :client
 
+    after_initialize :add_date_created
+
     def due_date
         self.date_created+7
     end
 
     def self.past_due_date
         self.all.select{|rental| rental.due_date < date.today}
+    end
+
+    private
+
+    def add_date_created
+        self.date_created = Date.today-rand(500) #In reality this should just be today's date, but for testing purposes: random past dates
     end
 
 end
